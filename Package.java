b80ff1724580bb6a46203701548c30e253d3e668
gercellosavio <gercellavoro@gmail.com>
2018-04-14 23:08:00 -0400

    se crearon los metodos (set y get de todos los atributos )y se creo la clase test sin ningun metodo o atributos es decir vacia

--- Package.java
+++ Package.java
@@ -3,7 +3,12 @@ public class Package{
  
   double valor_pago;
   Rental renta = new Rental();
-  valor_pago=renta.Calcular_precio(0,4.00,1);
+  renta.setopcion(1);
+  renta.setcantidad_bicycle(4);
+  renta.setvalor_de_pago(4.00);
+  renta.Calcular_precio();
+
+  valor_pago=renta.gettotal_a_pagar();
     System.out.println("Valor a Pagar:"+valor_pago+"$");
  }
 }
--- Rental.java
+++ Rental.java
@@ -1,17 +1,78 @@
 public class Rental{
 	//ATRIBUTOS 
- private int opcion; // saber si es hora,dia,semana
+ private int opcion; // saber si es hora=(0),dia=(1),semana=(2)
  private double total_a_pagar=0.00;//valor a pagar por el cliente 
- private double valor_de_pago;//cantida de horas ,dia, semanas
- private int cantidad_bicycle;//cantidad de bicicletas rentadas 
+ private int cantidad_bicycle;
+ private double valor_de_pago=0.00;
    //METODOS
- public double Calcular_precio(int opcion,double valor_de_pago,int cantidad_bicycle){    //metodo para calcular el precio a pagar por el cliente 
- 	 
+ public void setopcion(int opcion){
+    this.opcion=opcion;
+ }
+
+ public int getopcion(){
+ 	return this.opcion;
+ }
+
+ public double gettotal_a_pagar(){
+ 	return this.total_a_pagar;
+ }
+
+ public void setcantidad_bicycle(int cantidad_bicycle){
+ 	this.cantidad_bicycle=cantidad_bicycle;
+ }
+
+ public int getcantidad_bicycle(){
+ 	return this.cantidad_bicycle;
+ }
+
+ public void setvalor_de_pago(double valor_de_pago){
+ 	this.valor_de_pago=valor_de_pago;
+ }
+
+public double getvalor_de_pago(){
+	return this.valor_de_pago;
+}
+
+ public void Calcular_precio(){    //metodo para calcular el precio a pagar por el cliente 
+ 	 double descuento;
     // condicion para calcular el monto a pagar por hora sin descuento
- 	if (opcion==0 && (cantidad_bicycle<3 || cantidad_bicycle>5)) {
-	  this.total_a_pagar=valor_de_pago*5;
+ 	if (this.opcion==0 && (this.cantidad_bicycle<3 || this.cantidad_bicycle>5)) {
+	  this.total_a_pagar=this.valor_de_pago*5;
 	 
  	}
-    return this.total_a_pagar;
+ 	 // condicion para calcular el monto a pagar por dia sin descuento
+ 	if (this.opcion==1 && (this.cantidad_bicycle<3 || this.cantidad_bicycle>5)) {
+ 		this.total_a_pagar=this.valor_de_pago*20;
+ 		
+ 	}
+    // condicion para calcular el monto a pagar por semana sin descuento
+ 	if (this.opcion==2 && (this.cantidad_bicycle<3 || this.cantidad_bicycle>5)) {
+ 		this.total_a_pagar=this.valor_de_pago*60;
+ 		
+ 	}
+ 	 // condicion para calcular el monto a pagar con el descuento del 30% de descuento
+ 	if  (this.cantidad_bicycle>=3 || this.cantidad_bicycle<=5) {
+ 		   // condicion para calcular el monto a pagar por hora con descuento
+ 		if (this.opcion==0 ) {
+	    this.total_a_pagar=this.valor_de_pago*5;
+	    descuento=this.valor_de_pago*5*0.3;
+	    this.total_a_pagar=this.total_a_pagar-descuento;
+	  }
+ 	     // condicion para calcular el monto a pagar por dia con descuento
+ 	    if (this.opcion==1 ) {
+ 		this.total_a_pagar=this.valor_de_pago*20;
+ 		descuento=this.valor_de_pago*20*0.3;
+ 		this.total_a_pagar=this.total_a_pagar-descuento;
+ 	  }
+         // condicion para calcular el monto a pagar por semana con descuento
+ 	if (this.opcion==2 ) {
+ 		this.total_a_pagar=this.valor_de_pago*60;
+ 		descuento=this.valor_de_pago*60*0.3;
+ 		this.total_a_pagar=this.total_a_pagar-descuento;
+ 	  }
+
+
+ 		
+ 	}
   }
 }
